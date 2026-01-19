f173782b2b92fde0e639ff596e8527b5c8ce6669
hsb-ini-14 <bhaduria14ini@gmail.com>
2026-01-19 12:14:30 +0530

    fixed minor bugs

--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,10 @@
-export const images = [
-  "/images/image-1.jpg",
-  "/images/image-2.jpg",
-  "/images/image-3.jpg",
-  "/images/image-4.jpg",
-  "/images/image-5.jpg",
-];
+import img1 from "../assets/image-1.jpg/";
+import img2 from "../assets/image-2.jpg/";
+import img3 from "../assets/image-3.jpg/";
+import img4 from "../assets/image-4.jpg/";
+import img5 from "../assets/image-5.jpg/";
+
+export const images = [img1, img2, img3, img4, img5];
 
 export const descriptions = [
   "Small🐶 breeds like Chihuahuas, Pomeranians, or Dachshunds are known for their playful, energetic, and loyal nature. Despite their size, many small dogs have big personalities and love attention. They make great companions and are often affectionate with their owners.",
